905b1dbbe9d1fc00e08555d3575ec50efbd81925
Oleksandr Glotov <alexander.v.glotov@gmail.com>
2023-03-24 12:26:09 +0200

    Initialized Project

---
+++ src/test/kotlin/ua/wwind/sfgjms/SfgJmsApplicationTests.kt
@@ -0,0 +1,13 @@
+package ua.wwind.sfgjms
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SfgJmsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
